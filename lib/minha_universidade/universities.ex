b48b88057cdcd32a1f1bb5e5790831f5b27829ae
otaviouu <otavio.venancio.oliveira@usp.br>
2026-02-09 02:45:31 -0300

    interface to get teacher_discipline

--- lib/minha_universidade/universities.ex
+++ lib/minha_universidade/universities.ex
@@ -14,7 +14,9 @@ defmodule MinhaUniversidade.Universities do
     resource MinhaUniversidade.Universities.Review
 
     resource MinhaUniversidade.Universities.TeacherDiscipline do
-      define :list_teacher_discipline_reviews, action: :read
+      define :list_teacher_discipline, action: :read
+      define :get_teacher_discipline_by_teacher_id, action: :read, get_by: [:teacher_id]
+      define :get_teacher_discipline_by_discipline_id, action: :read, get_by: [:discipline_id]
     end
 
     resource MinhaUniversidade.Universities.Faculty
