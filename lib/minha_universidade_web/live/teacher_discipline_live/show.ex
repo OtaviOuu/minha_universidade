5356917dea72d72af20c115966022655b717639a
otaviouu <otavio.venancio.oliveira@usp.br>
2026-02-10 00:58:29 -0300

    modal for review

--- lib/minha_universidade_web/live/teacher_discipline_live/show.ex
+++ lib/minha_universidade_web/live/teacher_discipline_live/show.ex
@@ -71,34 +71,77 @@ defmodule MinhaUniversidadeWeb.TeacherDisciplineLive.Show do
     ~H"""
     <ul class="list bg-base-100 rounded-box shadow-md">
       <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Most played songs this week</li>
-      <.disciplines_teacher_row :for={review <- @reviews} review={review} />
+      <.disciplines_teacher_row
+        :for={review <- @reviews}
+        review={review}
+      />
     </ul>
     """
   end
 
   def disciplines_teacher_row(assigns) do
     ~H"""
-    <li class="list-row">
-      <div>
-        <img
-          class="size-10 rounded-box"
-          src="https://img.daisyui.com/images/profile/demo/1@94.webp"
-        />
+    <li class="list-row flex flex-col gap-2 cursor-pointer hover:bg-base-200 rounded-box p-4">
+      <div class="modal" role="dialog" id={"review-#{@review.id}"}>
+        <div class="modal-box w-11/12 max-w-5xl">
+          <.review_modal_contend review={@review} />
+          <div class="modal-action">
+            <a href="#" class="btn">Yay!</a>
+          </div>
+        </div>
       </div>
-      <div>
-        <div>Dio Lupa</div>
-        <div class="text-xs uppercase font-semibold opacity-60">Remaining Reason</div>
-      </div>
-      <p class="list-col-wrap text-xs">
-        {@review.geral_comments}
-      </p>
-      <button class="btn btn-square btn-ghost">
-        <.icon name="hero-hand-thumb-up" class="size-[1.2em]" />
-      </button>
-      <button class="btn btn-square btn-ghost">
-        <.icon name="hero-hand-thumb-down" class="size-[1.2em]" />
-      </button>
+      <.link href={"#review-#{@review.id}"}>
+        <div class="flex items-center justify-between">
+          <div class="flex items-center gap-3">
+            <img
+              class="size-10 rounded-box"
+              src="https://img.daisyui.com/images/profile/demo/1@94.webp"
+            />
+
+            <div>
+              <div>Dio Lupa</div>
+
+              <div class="flex gap-2 text-xs uppercase font-semibold opacity-60">
+                <div class="badge badge-sm badge-primary">
+                  Didática: {@review.didactics_rate}
+                </div>
+
+                <div class="badge badge-sm badge-primary">
+                  Avaliações: {@review.exams_rate}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="badge badge-primary">
+            Geral: {@review.geral_rating}
+          </div>
+        </div>
+
+        <p class="text-xs opacity-80">
+          {@review.geral_comments}
+        </p>
+      </.link>
     </li>
+    """
+  end
+
+  attr :review, :map, required: true
+
+  def review_modal_contend(assigns) do
+    ~H"""
+    <.header>
+      Avaliação aa
+      <:subtitle>
+        Avaliação detalhada para ajudar outros estudantes a escolherem as melhores opções para suas jornadas acadêmicas.
+      </:subtitle>
+      <:actions>
+        <.icon name="hero-hand-thumb-up" />
+      </:actions>
+      <:actions>
+        <.icon name="hero-hand-thumb-up" />
+      </:actions>
+    </.header>
     """
   end
 end
